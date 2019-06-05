f47869f84a0e886339cba51c2e4970de6fb62e2b
David <david.martin@est.tech>
2019-06-05 14:49:11 +0100

    refactoring a cleaning

--- src/main/java/com/patterns_playground/proxy/User.java
+++ src/main/java/com/patterns_playground/proxy/User.java
@@ -5,38 +5,38 @@ import org.springframework.stereotype.Component;
 @Component
 class User {
 
-    private String username;
-    private String password;
-    
-    public User() {
+	private String username;
+	private String password;
+
+	public User() {
 		this.username = "";
 		this.password = "";
-    }
-    
-    public User(String username, String password) {
+	}
+
+	public User(String username, String password) {
 		this.username = username;
 		this.password = password;
 	}
 
 	public String getUsername() {
-        return username;
-    }
+		return username;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    @Override
-    public String toString() {
-        return "User [password=" + password + ", username=" + username + "]";
-    }
+	@Override
+	public String toString() {
+		return "User [password=" + password + ", username=" + username + "]";
+	}
 
 }
